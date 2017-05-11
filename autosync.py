73738ce712e6761c59e4aba562aee466614857b2
Erik <erik.eriksson@yahoo.com>
2017-05-11 14:08:24 +0200

    initial checkin

---
+++ autosync.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import logging
+from sys import argv
+from time import sleep
+from lms import find_server
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def _autosync(players):
+    # if some other player is playing same content/stream,
+    # then auto sync them
+    for player in [p for p in players
+                   if p.is_playing and
+                   not p.is_synced]:
+
+        for other in [p for p in players
+                      if p.is_playing and
+                      p != player and
+                      p.track_id == player.track_id]:
+
+            _LOGGER.info('Autosyncing %s to %s', player.name, other.name)
+            other.sync_to(player)
+            return
+
+
+if __name__ == '__main__':
+    if '-vv' in argv:
+        logging.basicConfig(level=logging.DEBUG)
+    elif '-v' in argv:
+        logging.basicConfig(level=logging.INFO)
+
+    server = find_server()
+    if not server:
+        exit('Server not found')
+    while True:
+        sleep(0.5)
+        _LOGGER.debug('Checking')
+        server.update_players()
+        _autosync(server.players)
