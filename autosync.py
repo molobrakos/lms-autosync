0e57f7fd3bf5a06af21cabffb8c102c09a6d07d3
Erik <erik.eriksson@yahoo.com>
2017-09-22 17:34:28 +0200

    muting

--- autosync.py
+++ autosync.py
@@ -11,8 +11,17 @@ _LOGGER = logging.getLogger(__name__)
 def _autosync(players):
     # mute all synced inactive players to
     # avoid surprises when starting playing again
+    for player in players:
+        _LOGGER.info('%20s: %s %d%%%s', player.name,
+                     player.track_id if player.is_playing else 'idle',
+                     player.volume, '(mute)' if player.is_muted else '')
+
     for player in [p for p in players
-                   if p.is_synced and not p.is_playing]:
+                   if p.is_synced
+                   and not p.is_playing
+                   and not p.is_muted
+                   and p.volume != 0]:
+        _LOGGER.info('Muting %s', player.name)
         player.mute()
 
     # if some other player is playing same content/stream,
@@ -33,9 +42,12 @@ def _autosync(players):
 
 if __name__ == '__main__':
     if '-vv' in argv:
-        logging.basicConfig(level=logging.DEBUG)
+        level = logging.DEBUG
     elif '-v' in argv:
-        logging.basicConfig(level=logging.INFO)
+        level = logging.INFO
+    else:
+        level = logging.ERROR
+    logging.basicConfig(level=level, format='%(asctime)s %(name)s: %(message)s', datefmt='%H:%M:%S')
 
     server = find_server()
     if not server:
